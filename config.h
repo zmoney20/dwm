fccfa8ffaa2c43983e9cde72e2b2de0e81951b61
Zach <zachranderson@proton.me>
2025-05-31 11:05:27 -0600

    autostart patch, added passmenu to keybinds

--- config.h
+++ config.h
@@ -93,12 +93,14 @@ static const char *dmenucmd[] = {
     "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL};
 static const char *termcmd[] = {"wezterm", NULL};
 static const char *browsercmd[] = {"qutebrowser", NULL};
+static const char *passcmd[] = {"passmenu", NULL};
 
 static const Key keys[] = {
     /* modifier, key, function, argument */
     {MODKEY, XK_space, spawn, {.v = dmenucmd}},
     {MODKEY, XK_Return, spawn, {.v = termcmd}},
     {MODKEY, XK_e, spawn, {.v = browsercmd}},
+    {MODKEY, XK_p, spawn, {.v = passcmd}},
     {MODKEY, XK_b, togglebar, {0}},
     {MODKEY, XK_j, focusstack, {.i = +1}},
     {MODKEY, XK_k, focusstack, {.i = -1}},
