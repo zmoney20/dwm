a30c2f900d1d45e9023e04430fb82269966d35fc
Zach <zachranderson@proton.me>
2025-05-31 18:23:14 -0600

    movestack patch

--- config.def.h
+++ config.def.h
@@ -96,6 +96,7 @@ static const char *browsercmd[] = {"qutebrowser", NULL};
 static const char *passcmd[] = {"passmenu", NULL};
 static const char *rofipasscmd[] = {"rofi-pass", NULL};
 
+#include "movestack.c"
 static const Key keys[] = {
     /* modifier, key, function, argument */
     {MODKEY, XK_space, spawn, {.v = dmenucmd}},
@@ -106,6 +107,8 @@ static const Key keys[] = {
     {MODKEY, XK_b, togglebar, {0}},
     {MODKEY, XK_j, focusstack, {.i = +1}},
     {MODKEY, XK_k, focusstack, {.i = -1}},
+    {MODKEY | ShiftMask, XK_j, movestack, {.i = +1}},
+    {MODKEY | ShiftMask, XK_k, movestack, {.i = -1}},
     {MODKEY, XK_i, incnmaster, {.i = +1}},
     {MODKEY, XK_d, incnmaster, {.i = -1}},
     {MODKEY, XK_h, setmfact, {.f = -0.05}},
