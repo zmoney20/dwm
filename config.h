2b2300c4a6542fd41de34061daf2d40cd7e2adfc
Zach <zachranderson@proton.me>
2025-06-11 14:14:03 -0600

    brave
    trying out ghostty
    
    Revert "trying out ghostty"
    
    This reverts commit 285a1cc19a7a855f0ee1e5cd3043a358e6964d9b.

--- config.h
+++ config.h
@@ -94,7 +94,7 @@ static const char *dmenucmd[] = {"dmenu_run", "-m",  dmenumon,      "-fn",
                                  col_gray3,   "-sb", col_cyan, "-sf",
                                  col_gray4,   NULL};
 static const char *termcmd[] = {"st", NULL};
-static const char *browsercmd[] = {"zen-browser", NULL};
+static const char *browsercmd[] = {"brave", NULL};
 static const char *passcmd[] = {"passmenu", NULL};
 static const char *rofipasscmd[] = {"rofi-pass", NULL};
 /* dmenu_sys - view running processes and choose to kill a certain pid */
