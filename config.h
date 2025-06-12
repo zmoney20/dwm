81b96e01cd47b86a6efecd20d498a9e02735f7f4
Zach <zachranderson@proton.me>
2025-06-11 21:28:17 -0600

    trying out ghostty
    This reverts commit 096cb7d830d89426ab88b44991ed059edd7854b0.

--- config.h
+++ config.h
@@ -93,7 +93,7 @@ static const char *dmenucmd[] = {"dmenu_run", "-m",  dmenumon,      "-fn",
                                  dmenufont,   "-nb", col_gray1,     "-nf",
                                  col_gray3,   "-sb", col_cyan, "-sf",
                                  col_gray4,   NULL};
-static const char *termcmd[] = {"st", NULL};
+static const char *termcmd[] = {"ghostty", NULL};
 static const char *browsercmd[] = {"brave", NULL};
 static const char *passcmd[] = {"passmenu", NULL};
 static const char *rofipasscmd[] = {"rofi-pass", NULL};
