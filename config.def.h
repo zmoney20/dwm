3c94093e07abf30f987cf5d79f86e28af692e15e
Zach <zachranderson@proton.me>
2025-06-16 20:51:34 -0600

    maple mono nf cn

--- config.def.h
+++ config.def.h
@@ -8,8 +8,8 @@ static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
 static const int user_bh =
     10; /* 2 is the default spacing around the bar's font */
-static const char *fonts[] = {"cozette:size=16"};
-static const char dmenufont[] = "cozette:size=16";
+static const char *fonts[] = {"MapleMonoNFCN:size=12"};
+static const char dmenufont[] = "MapleMonoNFCN:size=12";
 static const char col_gray1[] = "#282828";
 static const char col_gray2[] = "#504945";
 static const char col_gray3[] = "#bdae93";
